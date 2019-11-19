8fc2f2963e4b702592c111c6f6a3900d6165120b
kbchojnacki <kbchojnacki@users.noreply.github.com>
2019-11-20 00:32:40 +0100

    Update benchmark.js

--- benchmark.js
+++ benchmark.js
@@ -1,6 +1,7 @@
 const util = require('util');
 const exec = util.promisify(require('child_process').exec);
 const os =  require('os');
+const process = require('process');
 
 async function bench() {
     const executions = 20;
@@ -10,7 +11,7 @@ async function bench() {
         // console.log(`${i} execution, avg: ${(new Date() - start)/(i+1)} ms`);
     }
     let time = new Date() - start;
-    console.log(`avg.: ${time/executions} ms, exec: ${executions}, cpu: ${os.cpus()[0].model}, os: ${os.platform()} ${os.release()}`)
+    console.log(`avg.: ${time/executions} ms, exec: ${executions}, cpu: ${os.cpus()[0].model}, node: ${process.version}, os: ${os.platform()} ${os.release()}`)
 
 }
 bench();
